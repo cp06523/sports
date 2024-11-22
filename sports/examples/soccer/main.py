f108ca504257337fa044c4dfa9ace17dbbd44a56
cp06523 <69167973+cp06523@users.noreply.github.com>
2024-11-22 17:52:37 -0500

    Update main.py
    Commented out lines 422-435

--- sports/examples/soccer/main.py
+++ sports/examples/soccer/main.py
@@ -419,16 +419,16 @@ def main(source_video_path: str, target_video_path: str, device: str, mode: Mode
         cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--source_video_path', type=str, required=True)
-    parser.add_argument('--target_video_path', type=str, required=True)
-    parser.add_argument('--device', type=str, default='cpu')
-    parser.add_argument('--mode', type=Mode, default=Mode.PLAYER_DETECTION)
-    args = parser.parse_args()
-    main(
-        source_video_path=args.source_video_path,
-        target_video_path=args.target_video_path,
-        device=args.device,
-        mode=args.mode
-    )
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='')
+#     parser.add_argument('--source_video_path', type=str, required=True)
+#     parser.add_argument('--target_video_path', type=str, required=True)
+#     parser.add_argument('--device', type=str, default='cpu')
+#     parser.add_argument('--mode', type=Mode, default=Mode.PLAYER_DETECTION)
+#     args = parser.parse_args()
+#     main(
+#         source_video_path=args.source_video_path,
+#         target_video_path=args.target_video_path,
+#         device=args.device,
+#         mode=args.mode
+#     )
